a725b3612f38447cf92d878b0e650d8b390207c7
Armstrong Subero <armstrongsubero@gmail.com>
2018-01-25 00:48:11 -0500

    Add files via upload

---
+++ x64_ASM/Pure_x64/Hello.asm
@@ -0,0 +1,80 @@
+; -------------------------------------------------------------------------
+; File: Hello.asm
+; Author: Armstrong Subero
+; uP: Intel Core i7 7500U @ 2.70GHz
+; Program: Hello
+; Compiler: (NASM 2.13.03rc1, GCC 5.1.0)
+; OS: Windows 10 64-bit
+; Program Version: 1.0
+;
+; Program Description: This Program Allows a processor to use assembly to
+;                      display text to an output window
+;
+; Created January 25th, 2018, 1:39 AM
+; ---------------------------------------------------------------------------
+
+; ---------------------------------------------------------------------------
+; Tell compiler to generate 64 bit code
+; ---------------------------------------------------------------------------
+bits 64
+
+; ---------------------------------------------------------------------------
+; Constants
+; ---------------------------------------------------------------------------
+NULL EQU 0
+STD_OUTPUT_HANDLE EQU -11
+
+; ---------------------------------------------------------------------------
+; Data segment:
+; ---------------------------------------------------------------------------
+section .data use64
+  Message: db 'hello!', 10
+  MessageLength  EQU $-Message
+
+; ---------------------------------------------------------------------------
+; Uninitialized data segment
+; ---------------------------------------------------------------------------
+section .bss
+alignb 8
+   StandardHandle resq 1
+   Written        resq 1
+
+; ---------------------------------------------------------------------------
+; Code segment:
+; ---------------------------------------------------------------------------
+section .text use64
+
+        global main
+        extern  ExitProcess
+        extern  GetStdHandle
+        extern  WriteFile
+
+; --------------------------------------------------------------------------
+; Start of Main Program
+; ---------------------------------------------------------------------------
+main:
+  and   RSP, 0FFFFFFFFFFFFFFF0h                  ; Align the stack to a multiple of 16 bytes
+
+  sub   RSP, 32                                  ; 32 bytes of shadow space
+  mov   RCX, STD_OUTPUT_HANDLE
+  call  GetStdHandle
+  mov   qword [REL StandardHandle], RAX
+  add   RSP, 32                                  ; Remove the 32 bytes
+
+  sub   RSP, 32 + 8 + 8                          ; Shadow space + 5th parameter + align stack
+                                                 ; to a multiple of 16 bytes
+  mov   RCX, qword [REL StandardHandle]          ; 1st parameter
+  lea   RDX, [REL Message]                       ; 2nd parameter
+  mov   R8, MessageLength                        ; 3rd parameter
+  lea   R9, [REL Written]                        ; 4th parameter
+  mov   qword [RSP + 4 * 8], NULL                ; 5th parameter
+  call  WriteFile                                ; Output can be redirect to a file using >
+  add   RSP, 48                                  ; Remove the 48 bytes
+
+  xor   RCX, RCX
+
+  ; stop from closing
+  here:
+          jmp here
+
+  call  ExitProcess
---
+++ x64_ASM/x64_Test/hello.asm
@@ -0,0 +1,65 @@
+; ---------------------------------------------------------------------------
+; Tell compiler to generate 64 bit code
+; ---------------------------------------------------------------------------
+bits 64
+
+; ---------------------------------------------------------------------------
+; Constants
+; ---------------------------------------------------------------------------
+NULL EQU 0
+STD_OUTPUT_HANDLE EQU -11
+
+; ---------------------------------------------------------------------------
+; Data segment:
+; ---------------------------------------------------------------------------
+section .data use64
+  Message: db 'hello!', 10
+  MessageLength  EQU $-Message
+
+; ---------------------------------------------------------------------------
+; Uninitialized data segment
+; ---------------------------------------------------------------------------
+section .bss
+alignb 8
+   StandardHandle resq 1
+   Written        resq 1
+
+; ---------------------------------------------------------------------------
+; Code segment:
+; ---------------------------------------------------------------------------
+section .text use64
+
+        global main
+        extern  ExitProcess
+        extern  GetStdHandle
+        extern  WriteFile
+
+; --------------------------------------------------------------------------
+; Start of Main Program
+; ---------------------------------------------------------------------------
+main:
+  and   RSP, 0FFFFFFFFFFFFFFF0h                  ; Align the stack to a multiple of 16 bytes
+
+  sub   RSP, 32                                  ; 32 bytes of shadow space
+  mov   RCX, STD_OUTPUT_HANDLE
+  call  GetStdHandle
+  mov   qword [REL StandardHandle], RAX
+  add   RSP, 32                                  ; Remove the 32 bytes
+
+  sub   RSP, 32 + 8 + 8                          ; Shadow space + 5th parameter + align stack
+                                                 ; to a multiple of 16 bytes
+  mov   RCX, qword [REL StandardHandle]          ; 1st parameter
+  lea   RDX, [REL Message]                       ; 2nd parameter
+  mov   R8, MessageLength                        ; 3rd parameter
+  lea   R9, [REL Written]                        ; 4th parameter
+  mov   qword [RSP + 4 * 8], NULL                ; 5th parameter
+  call  WriteFile                                ; Output can be redirect to a file using >
+  add   RSP, 48                                  ; Remove the 48 bytes
+
+  xor   RCX, RCX
+
+  ; stop from closing
+  here:
+          jmp here
+            
+  call  ExitProcess
